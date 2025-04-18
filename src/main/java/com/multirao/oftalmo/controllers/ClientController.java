79098d5b43eae129a0ac53f0925cbd7b87a4290a
Marcos <marcosmp1989@hotmail.com>
2025-04-17 23:02:27 -0300

    add login usuario

--- src/main/java/com/multirao/oftalmo/controllers/ClientController.java
+++ src/main/java/com/multirao/oftalmo/controllers/ClientController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,8 @@ import org.springframework.web.server.ResponseStatusException;
 import com.multirao.oftalmo.models.Client;
 import com.multirao.oftalmo.repository.ClientRepository;
 import org.springframework.web.bind.annotation.RequestParam;
+
+@PreAuthorize("hasAnyRole('ADMIN', 'ATENDENTE')")
 
 @RestController
 @RequestMapping(value = "/client")
